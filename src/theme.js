b33ad3e9683973c09ece3f06e0f82ff7eb35c879
YuriiPohorilets <pohorilets.y@gmail.com>
2023-06-11 18:45:44 +0300

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,38 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#30475E',
+      darker: '#222831',
+      light: '#DDDDDD',
+      accent: '#AA8B56',
+    },
+
+    neutral: {
+      darker: 'rgba(34, 40, 49, 0.4);',
+      accent: 'rgba(170, 139, 86, 0.4)',
+    },
+
+    media: {
+      linkedin: '#027BB5',
+      github: '#161B22',
+      skype: '#04ABEA',
+      telegram: '#20A0E1',
+    },
+  },
+
+  typography: {
+    fontFamily: ['Jost', 'sans-serif'].join(','),
+  },
+
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
